297781457f96276bde10d8641e8d5cd6348119e8
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2021-12-02 14:45:27 -0300

    Criando Links

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,34 +1,40 @@
-import React from 'react'
-import imagem from '../assets/img/doguito.svg'
-import '../assets/css/componentes/cabecalho.css'
+import React from "react";
+import imagem from "../assets/img/doguito.svg";
+import "../assets/css/componentes/cabecalho.css";
+import { Link } from "react-router-dom";
 
-const Cabecalho = () => { 
-  return(
+const Cabecalho = () => {
+  return (
     <header className="cabecalho container">
       <div className="menu-hamburger">
-        <span className="menu-hamburger__icone">
-        </span>
+        <span className="menu-hamburger__icone"></span>
       </div>
       <div className="cabecalho-container">
         <a href="/" className="flex flex--centro">
-          <img className="cabecalho__logo" src={imagem} alt="Logo Doguito"/>
+          <img className="cabecalho__logo" src={imagem} alt="Logo Doguito" />
           <h1 className="cabecalho__titulo">PetShop</h1>
         </a>
       </div>
 
       <nav className="menu-cabecalho">
         <ul className="menu-itens">
-          <li><a href="#" className="menu-item menu-item--entrar">Entrar</a></li>
-          <li><a href="#" className="menu-item">Produtos</a></li>
-          <li><a href="/" className="menu-item">Blog</a></li>
-          <li><a href="/sobre" className="menu-item">Sobre</a></li>
+          <li>
+            <Link to="/entrar" className="menu-item menu-item--entrar">Entrar</Link>
+          </li>
+          <li>
+          <Link to="/produtos" className="menu-item">Produtos</Link>
+          </li>
+          <li>
+          <Link to="/blog" className="menu-item">Blog</Link>
+          </li>
+          <li>
+            <Link to="/sobre" className="menu-item">Sobre</Link>
+          </li>
         </ul>
-
       </nav>
       <div className="menu-cabecalho-background"></div>
     </header>
-  )
-}
+  );
+};
 
-
-export default Cabecalho
+export default Cabecalho;
